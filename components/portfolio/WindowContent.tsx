d6781ae632c48f0aff4e500d548597a2088c04aa
Darkdemon75 <hemakarthik0507@gmail.com>
2026-06-12 13:20:09 +0530

    error fixings

--- components/portfolio/WindowContent.tsx
+++ components/portfolio/WindowContent.tsx
@@ -643,7 +643,7 @@ export function NotesWindowContent({ onNavigate }: { onNavigate?: (section: stri
       </motion.div> 
 
       {/* Quick Actions */}
-      <motion.div>
+      <motion.div
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.4 }}
